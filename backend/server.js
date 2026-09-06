9d4c12bec94cf97a9d046e8f181578d11696e244
navya0980 <navyakeerthi95532@gmail.com>
2026-09-06 17:37:50 +0530

    Update CORS origin to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ import orderRoute from "./routes/orderRoute.js"
 const PORT=process.env.PORT||3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'process.env.FRONTEND_URL',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true // if using cookies/auth
 }));
